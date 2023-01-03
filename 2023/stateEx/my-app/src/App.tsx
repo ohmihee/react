f14b34b9fa04e0459cd989195307ac99985f3582
mihee <algml9603@gmail.com>
2023-01-04 02:27:39 +0900

    2023 react

---
+++ 2023/stateEx/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>블로그</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
